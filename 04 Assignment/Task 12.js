2250a0dabaf85d1aec9e282f01451422bbd6883a
nprim13 <Nikkia@Nikkias-MacBook-Pro.local>
2016-12-04 12:09:29 -0500

    Adding Draft proposal and still am working on project 5

---
+++ 04 Assignment/Task 12.js
@@ -0,0 +1,14 @@
+
+
+var fruits = [
+				'apples', //0
+				'mangos', //1
+				'kiwis', //2
+				'strawberries', //3
+				'bananas', //4
+				'dates', //5
+				'oranges' //6 
+			];
+
+		var random = fruits[Math.floor(Math.random() * fruits.length)];
+		
